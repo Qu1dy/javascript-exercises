6febebd7741960e6b98e6fc0fe2db0c04ad49bf3
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-06-29 01:03:46 +0300

    yay fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if(num < 2){
+        return num;
+    }
+    if(num == 2){
+        return 1;
+    }
+    let twoBefore = 1;
+    let last = 1;
+    let next;
+    for(let i=2;i<num;i++) {
+        next = twoBefore + last;
+        twoBefore = last;
+        last = next;
+    }
+    return next;
 };
 
 // Do not edit below this line

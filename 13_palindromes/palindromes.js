e75fb7c1ca5c6ba2d1aaad48d0aab5c1adb5b4db
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-06-29 00:39:58 +0300

    add palindromes func

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,9 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.replace(/\W/g, '').toLowerCase();
+    let reversed = ''
+    for(let i=str.length-1; i>=0;i--)
+        reversed += str[i];
+    return str === reversed;
 
 };
 

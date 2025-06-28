17147d1b34acf36e0d04550ebd834c37b9f1c824
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-06-29 00:24:22 +0300

    add power, factorial, multiply funcs

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -10,16 +10,19 @@ const sum = function(arr) {
   return arr.reduce((prev, current) => prev+=current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((prev, current) => prev*=current, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let sum = 1;
+  for(let i=1;i<=a;i++)
+    sum*=i;
+  return sum;
 };
 
 // Do not edit below this line
